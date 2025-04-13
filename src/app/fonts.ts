28215e938e7216b01165c097afdba590fad5f7e9
Olivier PERDRIX <olivierperdrix@live.fr>
2025-04-13 17:56:19 +0200

    build: :building_construction: add packages and fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,17 @@
+import localFont from 'next/font/local';
+
+export const centuryGothic = localFont({
+  src: [
+    {
+      path: '../../public/fonts/centurygothic.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/centurygothic_bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
+import '@fontsource/inter';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const montserrat = Montserrat({
+  weight: ['300', '400', '500', '700'],
+  style: ['normal', 'italic'],
   subsets: ["latin"],
 });
 
@@ -23,9 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.className} antialiased`}
       >
         {children}
       </body>
