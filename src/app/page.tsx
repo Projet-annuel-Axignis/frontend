de2e2f7f3b468ac880c33d6b5a1b997675bacaaa
s-kenza <kenza.schuler@gmail.com>
2025-04-21 15:42:28 +0200

    ✨ feat: add technical base page and left menu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       title: t('homepage.services.items.2.title'),
       description: t('homepage.services.items.2.description'),
       imageSrc: "/images/services/service-3.jpg",
-      link: "#"
+      link: "/technical-base"
     }
   ];
 
