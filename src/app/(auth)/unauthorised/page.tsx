3fc7fba729f8a33d77bf7ca49827bf227b3ec58a
Olivier PERDRIX <olivierperdrix@live.fr>
2025-04-29 17:44:59 +0200

    fix: update error translation pages

--- src/app/(auth)/unauthorised/page.tsx
+++ src/app/(auth)/unauthorised/page.tsx
@@ -65,11 +65,11 @@ export default function UnauthorisedPage() {
         </Typography>
 
         <Typography level="h3" className="text-xl mb-2 text-gray-900 dark:text-white">
-          {t('error.unauthorised_title')}
+          {t('error_unauthorised.unauthorised_title')}
         </Typography>
 
         <Typography className="mb-8 text-gray-600 dark:text-gray-400">
-          {t('error.unauthorised_message')}
+          {t('error_unauthorised.unauthorised_message')}
         </Typography>
 
         <Box className="flex flex-col sm:flex-row gap-4 justify-center mt-5">
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,10 @@
+"use client"
+import { useTranslation } from '@/i18n/useTranslation';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function NotFound() {
+  const { t } = useTranslation("common")
   return (
     <main className="flex items-center justify-center min-h-screen bg-white dark:bg-gray-900 px-4">
       <div className='dark:hidden absolute top-0 left-0 w-full h-20 bg-gradient-to-b from-axignis-primary to-axignis-secondary opacity-50'></div>
@@ -17,16 +20,16 @@ export default function NotFound() {
         />
 
         <h1 className="text-4xl md:text-5xl font-bold text-gray-900 dark:text-white mb-4">
-          Oups ! Page non trouvée
+          {t("error404.title")}
         </h1>
 
         <p className="text-lg text-gray-600 mb-8">
-          La page que vous recherchez semble introuvable ou a été déplacée.
+          {t("error404.subtitle")}
         </p>
 
         <Link href="/">
           <button className="bg-axignis-primary hover:bg-axignis-secondary text-white font-semibold px-6 py-3 rounded-lg shadow-md transition">
-            Retour à l&apos;accueil
+            {t("error404.button_back")}
           </button>
         </Link>
       </div>
