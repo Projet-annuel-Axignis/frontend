470cdbf23e7c84bb833f0676d54654f54555d191
Olivier PERDRIX <olivierperdrix@live.fr>
2025-05-02 16:49:16 +0200

    feat: reorganize layouts

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,4 +1,4 @@
-import Home from '@/app/page';
+import Home from '@/app/(main-site)/page';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { ImageProps } from 'next/image';
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,6 @@
 "use client"
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 import { useTranslation } from '@/i18n/useTranslation';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -6,33 +8,37 @@ import Link from 'next/link';
 export default function NotFound() {
   const { t } = useTranslation("common")
   return (
-    <main className="flex items-center justify-center min-h-screen bg-white dark:bg-gray-900 px-4">
-      <div className='dark:hidden absolute top-0 left-0 w-full h-20 bg-gradient-to-b from-axignis-primary to-axignis-secondary opacity-50'></div>
+    <>
+      <Header />
+      <main className="flex items-center justify-center min-h-screen bg-white dark:bg-gray-900 px-4">
+        <div className='dark:hidden absolute top-0 left-0 w-full h-20 bg-gradient-to-b from-axignis-primary to-axignis-secondary opacity-50'></div>
 
-      <div className="text-center max-w-xl">
-        <Image
-          src="/images/404.png"
-          alt="Page non trouvée"
-          width={400}
-          height={400}
-          priority
-          className="mx-auto mb-8"
-        />
+        <div className="text-center max-w-xl">
+          <Image
+            src="/images/404.png"
+            alt="Page non trouvée"
+            width={400}
+            height={400}
+            priority
+            className="mx-auto mb-8"
+          />
 
-        <h1 className="text-4xl md:text-5xl font-bold text-gray-900 dark:text-white mb-4">
-          {t("error404.title")}
-        </h1>
+          <h1 className="text-4xl md:text-5xl font-bold text-gray-900 dark:text-white mb-4">
+            {t("error404.title")}
+          </h1>
 
-        <p className="text-lg text-gray-600 mb-8">
-          {t("error404.subtitle")}
-        </p>
+          <p className="text-lg text-gray-600 mb-8">
+            {t("error404.subtitle")}
+          </p>
 
-        <Link href="/">
-          <button className="bg-axignis-primary hover:bg-axignis-secondary text-white font-semibold px-6 py-3 rounded-lg shadow-md transition">
-            {t("error404.button_back")}
-          </button>
-        </Link>
-      </div>
-    </main>
+          <Link href="/">
+            <button className="bg-axignis-primary hover:bg-axignis-secondary text-white font-semibold px-6 py-3 rounded-lg shadow-md transition">
+              {t("error404.button_back")}
+            </button>
+          </Link>
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 }
