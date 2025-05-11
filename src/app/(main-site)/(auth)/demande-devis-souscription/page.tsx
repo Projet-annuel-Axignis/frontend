2ccc9fc8720a54323de7ce77096fd99c6274ff7a
Olivier PERDRIX <olivierperdrix@live.fr>
2025-05-11 17:36:02 +0200

    refactor(config): désactiver temporairement certaines options de configuration pour l'export vers GitHub Pages et mise à jour de la gestion des paramètres de recherche dans la page d'inscription

--- src/app/(main-site)/(auth)/demande-devis-souscription/page.tsx
+++ src/app/(main-site)/(auth)/demande-devis-souscription/page.tsx
@@ -1,13 +1,13 @@
 import { Plans } from '@/types/plans';
 import RegisterForm from './RegisterForm';
 
-interface RegisterPageProps {
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
-export default function RegisterPage({ searchParams }: RegisterPageProps) {
-  const planParam = searchParams.p as string;
-  const initialPlan = planParam === Plans.SELF_MANAGED ? Plans.SELF_MANAGED : Plans.ADMIN_MANAGED;
+export default async function RegisterPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
+  const { p = Plans.ADMIN_MANAGED } = await searchParams;
+  const initialPlan = p === Plans.SELF_MANAGED ? Plans.SELF_MANAGED : Plans.ADMIN_MANAGED;
 
   return <RegisterForm initialPlan={initialPlan} />;
 }
