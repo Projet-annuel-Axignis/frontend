6f31c97aefd237d4ff34f7ba75503901b2e3d36d
Olivier PERDRIX <olivierperdrix@live.fr>
2025-05-11 17:36:02 +0200

    refactor(config): désactiver temporairement certaines options de configuration pour l'export vers GitHub Pages et mise à jour de la gestion des paramètres de recherche dans la page d'inscription

--- next.config.ts
+++ next.config.ts
@@ -8,16 +8,16 @@ const assetPrefix = process.env.NEXT_ASSET_PREFIX || '';
 const nextConfig: NextConfig = {
   /* config options here */
   // Configuration spécifique pour l'export vers GitHub Pages
-  output: 'export',
+  //output: 'export',
   // Ajoute le préfixe de chemin pour GitHub Pages si nécessaire
   basePath: basePath,
   assetPrefix: assetPrefix,
   // Désactive l'image optimization qui n'est pas compatible avec l'export statique
-  images: {
-    unoptimized: true,
-  },
+  // images: {
+  //   unoptimized: true,
+  // },
   // Ajoute le trailing slash pour la compatibilité avec GitHub Pages
-  trailingSlash: true,
+  //trailingSlash: true,
   transpilePackages: ['mui-tel-input'],
 };
 const withNextIntl = createNextIntlPlugin();
