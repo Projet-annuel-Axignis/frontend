9bbae12934cac5561cb47e672c72f2d25c9abf22
schuler <kenza.schuler@aprr.fr>
2025-05-14 14:32:54 +0200

    fix: :bug: correction layout de l'app B

--- src/app/(base-technique)/layout.tsx
+++ src/app/(base-technique)/layout.tsx
@@ -1,5 +1,6 @@
-import Header from '@/components/Header';
 import { Metadata } from 'next';
+import { CssVarsProvider, CssBaseline, Box } from '@mui/joy';
+import Sidebar from '@/components/Sidebar';
 
 export const metadata: Metadata = {
   title: "Axignis - Base technique de références",
@@ -12,8 +13,29 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <Header />
-      {children}
+    <CssVarsProvider disableTransitionOnChange>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', minHeight: '100dvh'}}>
+      <Sidebar />
+      <Box
+          component="main"
+          className="MainContent"
+          sx={{
+            px: { xs: 2, md: 6 },
+            pt: { xs: '30px', sm: '30px', md: '30px' },
+            pb: { xs: 2, sm: 2, md: 3 },
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            minWidth: 0,
+            height: '100dvh',
+            gap: 1,
+          }}
+          >
+            {children}
+          </Box>
+      </Box>
+    </CssVarsProvider>
     </>
   );
 }
