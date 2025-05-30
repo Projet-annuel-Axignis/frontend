a5d66246b9eb0873060838208cce1f2d5bd0f816
s-kenza <kenza.schuler@gmail.com>
2025-05-30 10:41:01 +0200

    refactor: :recycle: restructure du layout pour light/dark mode

--- src/app/(base-technique)/layout.tsx
+++ src/app/(base-technique)/layout.tsx
@@ -1,41 +1,38 @@
-import { Metadata } from 'next';
+'use client';
+
+import * as React from 'react';
 import { CssVarsProvider, CssBaseline, Box } from '@mui/joy';
 import Sidebar from '@/components/Sidebar';
 
-export const metadata: Metadata = {
-  title: "Axignis - Base technique de références",
-};
-
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <>
-    <CssVarsProvider defaultMode='system' disableTransitionOnChange>
-      <CssBaseline />
-      <Box sx={{ display: 'flex', minHeight: '100dvh'}}>
-      <Sidebar />
-      <Box
-          component="main"
-          className="MainContent"
-          sx={{
-            px: { xs: 2, md: 6 },
-            pt: { xs: '30px', sm: '30px', md: '30px' },
-            pb: { xs: 2, sm: 2, md: 3 },
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            minWidth: 0,
-            height: '100dvh',
-            gap: 1,
-          }}
-          >
-            {children}
+    <html lang="fr">
+      <body>
+        <CssVarsProvider defaultMode="system" disableTransitionOnChange>
+          <CssBaseline />
+          <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+            <Sidebar />
+            <Box
+              component="main"
+              sx={{
+                px: { xs: 2, md: 6 },
+                pt: '30px',
+                pb: { xs: 2, sm: 2, md: 3 },
+                flex: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                minWidth: 0,
+                height: '100dvh',
+                gap: 1,
+              }}
+            >
+              {children}
+            </Box>
           </Box>
-      </Box>
-    </CssVarsProvider>
-    </>
+        </CssVarsProvider>
+      </body>
+    </html>
   );
 }
