3bf013d2b1af69c97b741b21f81fa8f382aa8416
Olivier Perdrix <olivierperdrix@live.fr>
2025-06-15 19:19:05 +0200

    ✨ feat(auth): refactor LoginForm and RegisterForm to remove theme provider, enhance layout with background image, and improve form structure for better user experience

--- src/components/base-technique/Sidebar.tsx
+++ src/components/base-technique/Sidebar.tsx
@@ -14,7 +14,6 @@ import {
   Avatar,
   Box,
   Chip,
-  CssBaseline,
   Divider,
   GlobalStyles,
   IconButton,
@@ -24,7 +23,6 @@ import {
   ListItemText,
   Paper,
   styled,
-  ThemeProvider,
   Typography
 } from '@mui/material';
 
@@ -88,270 +86,266 @@ export default function Sidebar() {
   const isDarkMode = theme.palette.mode === 'dark';
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-
-      <StyledPaper
-        className="Sidebar"
-        elevation={0}
-      >
-        <GlobalStyles
-          styles={(theme) => ({
-            ':root': {
-              '--Sidebar-width': '220px',
-              [theme.breakpoints.up('lg')]: {
-                '--Sidebar-width': '240px',
-              },
+    <StyledPaper
+      className="Sidebar"
+      elevation={0}
+    >
+      <GlobalStyles
+        styles={(theme) => ({
+          ':root': {
+            '--Sidebar-width': '220px',
+            [theme.breakpoints.up('lg')]: {
+              '--Sidebar-width': '240px',
             },
-          })}
-        />
-        <Box
-          className="Sidebar-overlay"
-          sx={{
-            position: 'fixed',
-            zIndex: 9998,
-            top: 0,
-            left: 0,
-            width: '100vw',
-            height: '100vh',
-            opacity: 'var(--SideNavigation-slideIn)',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            transition: 'opacity 0.4s',
-            transform: {
-              xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1) + var(--SideNavigation-slideIn, 0) * var(--Sidebar-width, 0px)))',
-              lg: 'translateX(-100%)',
-            },
-          }}
-          onClick={() => closeSidebar()}
-        />
-        <div className=" mx-auto px-4 flex items-center justify-between">
-          {/* Logo */}
-          <Link href="/" className="relative h-12 w-32">
-            <Image
-              src={isDarkMode ? "/images/logo/logo-axignis-nb.png" : "/images/logo/logo-axignis.png"}
-              alt="Axignis Logo"
-              fill={true}
-              sizes="100%"
-              className={`
+          },
+        })}
+      />
+      <Box
+        className="Sidebar-overlay"
+        sx={{
+          position: 'fixed',
+          zIndex: 9998,
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          opacity: 'var(--SideNavigation-slideIn)',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          transition: 'opacity 0.4s',
+          transform: {
+            xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1) + var(--SideNavigation-slideIn, 0) * var(--Sidebar-width, 0px)))',
+            lg: 'translateX(-100%)',
+          },
+        }}
+        onClick={() => closeSidebar()}
+      />
+      <div className=" mx-auto px-4 flex items-center justify-between">
+        {/* Logo */}
+        <Link href="/" className="relative h-12 w-32">
+          <Image
+            src={isDarkMode ? "/images/logo/logo-axignis-nb.png" : "/images/logo/logo-axignis.png"}
+            alt="Axignis Logo"
+            fill={true}
+            sizes="100%"
+            className={`
               object-contain
               transition-opacity
             `}
-            />
-          </Link>
-        </div>
-        <div className="flex flex-col items-center justify-between w-full p-px">
-          <Chip
-            label="Base technique"
-            size="medium"
-            variant="filled"
-            color="primary"
-            sx={{ marginBottom: 2.5 }}
           />
-        </div>
-        <Box
+        </Link>
+      </div>
+      <div className="flex flex-col items-center justify-between w-full p-px">
+        <Chip
+          label="Base technique"
+          size="medium"
+          variant="filled"
+          color="primary"
+          sx={{ marginBottom: 2.5 }}
+        />
+      </div>
+      <Box
+        sx={{
+          minHeight: 0,
+          overflow: 'hidden auto',
+          flexGrow: 1,
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <List
+          dense
           sx={{
-            minHeight: 0,
-            overflow: 'hidden auto',
-            flexGrow: 1,
-            display: 'flex',
-            flexDirection: 'column',
+            gap: 1,
+            '& .MuiListItemButton-root': {
+              gap: 1.5,
+              borderRadius: 1,
+            },
           }}
         >
-          <List
-            dense
-            sx={{
-              gap: 1,
-              '& .MuiListItemButton-root': {
-                gap: 1.5,
-                borderRadius: 1,
-              },
-            }}
-          >
-            <ListItem disablePadding>
-              <ListItemButton>
-                <FolderIcon />
-                <ListItemText
-                  primary={
-                    <Typography variant="body2" fontWeight="medium">
-                      Domaines d&apos;équipements
-                    </Typography>
-                  }
-                />
-              </ListItemButton>
-            </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <FolderIcon />
+              <ListItemText
+                primary={
+                  <Typography variant="body2" fontWeight="medium">
+                    Domaines d&apos;équipements
+                  </Typography>
+                }
+              />
+            </ListItemButton>
+          </ListItem>
 
-            <ListItem disablePadding>
-              <ListItemButton>
-                <DashboardRoundedIcon />
-                <ListItemText
-                  primary={
-                    <Typography variant="body2" fontWeight="medium">
-                      Familles d&apos;équipements
-                    </Typography>
-                  }
-                />
-              </ListItemButton>
-            </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <DashboardRoundedIcon />
+              <ListItemText
+                primary={
+                  <Typography variant="body2" fontWeight="medium">
+                    Familles d&apos;équipements
+                  </Typography>
+                }
+              />
+            </ListItemButton>
+          </ListItem>
 
-            <ListItem disablePadding>
-              <ListItemButton>
-                <CableIcon />
-                <ListItemText
-                  primary={
-                    <Typography variant="body2" fontWeight="medium">
-                      Types d&apos;équipements
-                    </Typography>
-                  }
-                />
-              </ListItemButton>
-            </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <CableIcon />
+              <ListItemText
+                primary={
+                  <Typography variant="body2" fontWeight="medium">
+                    Types d&apos;équipements
+                  </Typography>
+                }
+              />
+            </ListItemButton>
+          </ListItem>
 
-            <ListItem disablePadding>
-              <ListItemButton>
-                <AppsIcon />
-                <ListItemText
-                  primary={
-                    <Typography variant="body2" fontWeight="medium">
-                      Marques
-                    </Typography>
-                  }
-                />
-              </ListItemButton>
-            </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <AppsIcon />
+              <ListItemText
+                primary={
+                  <Typography variant="body2" fontWeight="medium">
+                    Marques
+                  </Typography>
+                }
+              />
+            </ListItemButton>
+          </ListItem>
 
-            <ListItem disablePadding>
-              <Toggler
-                renderToggle={({ open, setOpen }) => (
-                  <ListItemButton onClick={() => setOpen(!open)}>
-                    <ShoppingCartRoundedIcon />
-                    <ListItemText
-                      primary={
-                        <Typography variant="body2" fontWeight="medium">
-                          Produits
-                        </Typography>
-                      }
-                    />
-                    <KeyboardArrowDownIcon
-                      sx={{
-                        transform: open ? 'rotate(180deg)' : 'none',
-                        transition: 'transform 0.2s',
-                      }}
-                    />
+          <ListItem disablePadding>
+            <Toggler
+              renderToggle={({ open, setOpen }) => (
+                <ListItemButton onClick={() => setOpen(!open)}>
+                  <ShoppingCartRoundedIcon />
+                  <ListItemText
+                    primary={
+                      <Typography variant="body2" fontWeight="medium">
+                        Produits
+                      </Typography>
+                    }
+                  />
+                  <KeyboardArrowDownIcon
+                    sx={{
+                      transform: open ? 'rotate(180deg)' : 'none',
+                      transition: 'transform 0.2s',
+                    }}
+                  />
+                </ListItemButton>
+              )}
+            >
+              <List sx={{ gap: 0.5, pl: 4 }}>
+                <ListItem sx={{ mt: 0.5 }} disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="Produits" />
                   </ListItemButton>
-                )}
-              >
-                <List sx={{ gap: 0.5, pl: 4 }}>
-                  <ListItem sx={{ mt: 0.5 }} disablePadding>
-                    <ListItemButton>
-                      <ListItemText primary="Produits" />
-                    </ListItemButton>
-                  </ListItem>
-                  <ListItem disablePadding>
-                    <ListItemButton>
-                      <ListItemText primary="Types de documents de produits" />
-                    </ListItemButton>
-                  </ListItem>
-                  <ListItem disablePadding>
-                    <ListItemButton>
-                      <ListItemText primary="Documents de produits" />
-                    </ListItemButton>
-                  </ListItem>
-                </List>
-              </Toggler>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <AppsIcon />
-                <ListItemText
-                  primary={
-                    <Typography variant="body2" fontWeight="medium">
-                      Marques
-                    </Typography>
-                  }
-                />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <Toggler
-                renderToggle={({ open, setOpen }) => (
-                  <ListItemButton onClick={() => setOpen(!open)}>
-                    <ShoppingCartRoundedIcon />
-                    <ListItemText
-                      primary={
-                        <Typography variant="body2" fontWeight="medium">
-                          Produits
-                        </Typography>
-                      }
-                    />
-                    <KeyboardArrowDownIcon
-                      sx={{
-                        transform: open ? 'rotate(180deg)' : 'none',
-                        transition: 'transform 0.2s',
-                      }}
-                    />
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="Types de documents de produits" />
                   </ListItemButton>
-                )}
-              >
-                <List sx={{ gap: 0.5, pl: 4 }}>
-                  <ListItem sx={{ mt: 0.5 }} disablePadding>
-                    <ListItemButton>
-                      <ListItemText primary="Produits" />
-                    </ListItemButton>
-                  </ListItem>
-                  <ListItem disablePadding>
-                    <ListItemButton>
-                      <ListItemText primary="Types de documents de produits" />
-                    </ListItemButton>
-                  </ListItem>
-                  <ListItem disablePadding>
-                    <ListItemButton>
-                      <ListItemText primary="Documents de produits" />
-                    </ListItemButton>
-                  </ListItem>
-                </List>
-              </Toggler>
-            </ListItem>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="Documents de produits" />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </Toggler>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <AppsIcon />
+              <ListItemText
+                primary={
+                  <Typography variant="body2" fontWeight="medium">
+                    Marques
+                  </Typography>
+                }
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <Toggler
+              renderToggle={({ open, setOpen }) => (
+                <ListItemButton onClick={() => setOpen(!open)}>
+                  <ShoppingCartRoundedIcon />
+                  <ListItemText
+                    primary={
+                      <Typography variant="body2" fontWeight="medium">
+                        Produits
+                      </Typography>
+                    }
+                  />
+                  <KeyboardArrowDownIcon
+                    sx={{
+                      transform: open ? 'rotate(180deg)' : 'none',
+                      transition: 'transform 0.2s',
+                    }}
+                  />
+                </ListItemButton>
+              )}
+            >
+              <List sx={{ gap: 0.5, pl: 4 }}>
+                <ListItem sx={{ mt: 0.5 }} disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="Produits" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="Types de documents de produits" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="Documents de produits" />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </Toggler>
+          </ListItem>
 
-          </List>
-          <List
-            dense
-            sx={{
-              mt: 'auto',
-              flexGrow: 0,
-              mb: 2,
-              '& .MuiListItemButton-root': {
-                borderRadius: 1,
-              },
-            }}
-          >
-            <ListItem disablePadding>
-              <ListItemButton>
-                <SupportRoundedIcon />
-                <ListItemText primary="Support" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <SettingsRoundedIcon />
-                <ListItemText primary="Settings" />
-              </ListItemButton>
-            </ListItem>
-          </List>
+        </List>
+        <List
+          dense
+          sx={{
+            mt: 'auto',
+            flexGrow: 0,
+            mb: 2,
+            '& .MuiListItemButton-root': {
+              borderRadius: 1,
+            },
+          }}
+        >
+          <ListItem disablePadding>
+            <ListItemButton>
+              <SupportRoundedIcon />
+              <ListItemText primary="Support" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <SettingsRoundedIcon />
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+      <Divider />
+      <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+        <Avatar
+          sx={{ width: 32, height: 32 }}
+          src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
+        />
+        <Box sx={{ minWidth: 0, flex: 1 }}>
+          <Typography variant="body2" fontWeight="medium">Loïc Rome</Typography>
         </Box>
-        <Divider />
-        <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
-          <Avatar
-            sx={{ width: 32, height: 32 }}
-            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
-          />
-          <Box sx={{ minWidth: 0, flex: 1 }}>
-            <Typography variant="body2" fontWeight="medium">Loïc Rome</Typography>
-          </Box>
-          <IconButton size="small" color="inherit">
-            <LogoutRoundedIcon />
-          </IconButton>
-        </Box>
-      </StyledPaper>
-    </ThemeProvider>
+        <IconButton size="small" color="inherit">
+          <LogoutRoundedIcon />
+        </IconButton>
+      </Box>
+    </StyledPaper>
   );
 }
