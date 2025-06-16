96ae1f6aa815315db5faf7a34aa04f724b590422
Olivier Perdrix <olivierperdrix@live.fr>
2025-06-16 20:39:26 +0200

    ✨ feat(Dashboard): implement DashBoardHeader component for consistent header styling across pages

--- src/app/dashboard/(base-technique)/produits/page.tsx
+++ src/app/dashboard/(base-technique)/produits/page.tsx
@@ -1,8 +1,8 @@
+import DashBoardHeader from '@/components/dashboard/DashBoardHeader';
 import DownloadButton from '@/components/dashboard/DownloadButton';
 import OrderTable from '@/components/dashboard/OrderTable';
 import InventoryIcon from '@mui/icons-material/Inventory';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -14,39 +14,12 @@ export default function JoyOrderDashboardTemplate() {
   return (
     <>
       {/* Header */}
-      <Box
-        sx={{
-          display: 'flex',
-          mb: 4,
-          gap: 2,
-          flexDirection: { xs: 'column', sm: 'row' },
-          alignItems: { xs: 'start', sm: 'center' },
-          justifyContent: 'space-between',
-        }}
+      <DashBoardHeader
+        title="Produits"
+        icon={<InventoryIcon />}
       >
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-          <InventoryIcon
-            sx={{
-              color: 'var(--color-axignis-primary)',
-              fontSize: '2rem'
-            }}
-          />
-          <Typography
-            variant="h3"
-            component="h1"
-            sx={{
-              fontWeight: 700,
-              background: `linear-gradient(135deg, var(--color-axignis-primary), var(--color-axignis-secondary))`,
-              backgroundClip: 'text',
-              WebkitBackgroundClip: 'text',
-              WebkitTextFillColor: 'transparent',
-            }}
-          >
-            Produits
-          </Typography>
-        </Box>
         <DownloadButton />
-      </Box>
+      </DashBoardHeader>
 
       {/* Content */}
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
--- src/app/dashboard/administration/page.tsx
+++ src/app/dashboard/administration/page.tsx
@@ -1,11 +1,18 @@
-import ProtectedRoute from "@/components/auth/ProtectedRoute"
+import ProtectedRoute from "@/components/auth/ProtectedRoute";
+import DashBoardHeader from "@/components/dashboard/DashBoardHeader";
+import { AdminPanelSettings } from "@mui/icons-material";
 
 const AdminPage = () => {
   return (
     <ProtectedRoute
       allowedRoles={['ADMINISTRATOR']}
     >
-      <div>AdminPage</div>
+      {/* Header */}
+      <DashBoardHeader
+        title="Administration"
+        icon={<AdminPanelSettings />}
+      />
+
     </ProtectedRoute>
   )
 }
