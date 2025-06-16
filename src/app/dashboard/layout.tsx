c23c3adb07163927e605cf1fc7ee1fbdabe7c0f6
Olivier Perdrix <olivierperdrix@live.fr>
2025-06-16 19:27:35 +0200

    ✨ feat(Dashboard): integrate ProtectedRoute for role-based access control in dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 'use client';
+import ProtectedRoute from '@/components/auth/ProtectedRoute';
 import BreadCrumb from '@/components/dashboard/BreadCrumb';
 import Sidebar from '@/components/dashboard/Sidebar';
 import { Box } from '@mui/material';
@@ -10,28 +11,32 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
-        <Sidebar />
-        <Box
-          component="main"
-          sx={{
-            px: { xs: 2, md: 6 },
-            pt: '30px',
-            pb: { xs: 2, sm: 2, md: 3 },
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            minWidth: 0,
-            height: '100dvh',
-            gap: 1,
-          }}
-        >
-          <Box sx={{ p: 3 }}>
-            <BreadCrumb />
-            {children}
+      <ProtectedRoute
+        allowedRoles={['ADMINISTRATOR', 'COMPANY_ADMINISTRATOR', 'COMPANY_MANAGER', 'COMPANY_MEMBER']}
+      >
+        <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+          <Sidebar />
+          <Box
+            component="main"
+            sx={{
+              px: { xs: 2, md: 6 },
+              pt: '30px',
+              pb: { xs: 2, sm: 2, md: 3 },
+              flex: 1,
+              display: 'flex',
+              flexDirection: 'column',
+              minWidth: 0,
+              height: '100dvh',
+              gap: 1,
+            }}
+          >
+            <Box sx={{ p: 3 }}>
+              <BreadCrumb />
+              {children}
+            </Box>
           </Box>
         </Box>
-      </Box>
+      </ProtectedRoute>
     </>
   );
 }
