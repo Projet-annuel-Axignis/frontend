8153036803878e2a0ffeba9a0d58fed73085909c
Olivier Perdrix <olivierperdrix@live.fr>
2025-06-16 21:29:29 +0200

    ✨ feat(Dashboard): add metadata to product page and enhance DashBoardHeader with optional chip and orientation props

--- src/components/dashboard/DashBoardHeader.tsx
+++ src/components/dashboard/DashBoardHeader.tsx
@@ -1,36 +1,59 @@
-import { Box, Typography } from "@mui/material";
+import { Box, ChipProps, Typography } from "@mui/material";
 import { SvgIconProps } from "@mui/material/SvgIcon";
 import React from "react";
 
-const DashBoardHeader = ({ title, icon, children }: { title: string, icon: React.ReactElement<SvgIconProps>, children?: React.ReactNode }) => {
+const DashBoardHeader = (
+  {
+    title,
+    icon = null,
+    chip = null,
+    orientation = 'row',
+    titleVariant = 'h3',
+    children
+  }: {
+    title: string,
+    icon?: React.ReactElement<SvgIconProps> | null,
+    chip?: React.ReactElement<ChipProps> | null,
+    titleVariant?: 'h3' | 'h4' | 'h5' | 'h6',
+    orientation?: 'row' | 'column',
+    children?: React.ReactNode
+  }) => {
+
   // Clone l'icône et applique les styles automatiquement
-  const styledIcon = React.cloneElement(icon, {
+  const styledIcon = icon ? React.cloneElement(icon, {
     ...icon.props,
     sx: {
       color: 'var(--color-axignis-primary)',
       fontSize: '2rem',
       ...(icon.props.sx || {}), // Permet de surcharger les styles si nécessaire
     }
-  } as SvgIconProps);
+  } as SvgIconProps) : null;
 
   return (
     <>
       {/* Header */}
       <Box
-        sx={{
-          display: 'flex',
-          mb: 4,
-          gap: 2,
-          flexDirection: { xs: 'column', sm: 'row' },
-          alignItems: { xs: 'start', sm: 'center' },
-          justifyContent: 'space-between',
-        }
-        }
+        sx={
+          orientation === 'row' ? {
+            display: 'flex',
+            mb: 4,
+            gap: 2,
+            flexDirection: { xs: 'column', sm: 'row' },
+            alignItems: { xs: 'start', sm: 'center' },
+            justifyContent: 'space-between',
+          } : {
+            mb: 4,
+          }}
       >
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: 2,
+          mb: orientation === 'row' ? 0 : 2,
+        }}>
           {styledIcon}
           <Typography
-            variant="h3"
+            variant={titleVariant}
             component="h1"
             sx={{
               fontWeight: 700,
@@ -42,6 +65,7 @@ const DashBoardHeader = ({ title, icon, children }: { title: string, icon: React
           >
             {title}
           </Typography>
+          {chip}
         </Box>
         {children}
       </Box>
